768fe748e33efe1d7c7ec8f4c22b8e6b2146866a
森井 康太 <h1202019016@hiratagakuen.onmicrosoft.com>
2022-04-01 22:53:20 +0900

    電線が電源と接触してたら通電すりような処理を追加したつもり

--- 線でつなぐ（仮）/Assets/bonten/Conductor_Script.cs
+++ 線でつなぐ（仮）/Assets/bonten/Conductor_Script.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Conductor_Script : MonoBehaviour
 {
+    protected
+        bool energization = false;
+    public
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,33 @@ public class Conductor_Script : MonoBehaviour
     void Update()
     {
         
+    }
+
+    //
+    void OnCollisionStay(Collider c)
+    {
+        //電源と接触してるとき
+        if(c.CompareTag("Power_Supply"))
+        {
+            //通電変数をtrueにし、色を赤に変更
+            energization = true;
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        //電源と接触せずに導体と接触してるとき
+        else if(c.CompareTag("Conductor"))
+        {
+            //絶縁体と接触してるとき
+            if (c.CompareTag("Insulator"))
+            {
+                //通電変数をfalseにする
+                energization = false;
+            }
+            else
+            {
+                //通電変数をtrueにし、色を赤に変更
+                energization = true;
+                GetComponent<Renderer>().material.color = Color.red;
+            }
+        }
     }
 }
