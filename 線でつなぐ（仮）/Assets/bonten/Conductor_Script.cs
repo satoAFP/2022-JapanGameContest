692c18ebf9cd537ff762b9f7626147ecd635c306
DEV3-22\GA3 <h1202019016@hiratagakuen.onmicrosoft.com>
2022-04-13 14:53:59 +0900

    辰巳修正用

--- 線でつなぐ（仮）/Assets/bonten/Conductor_Script.cs
+++ 線でつなぐ（仮）/Assets/bonten/Conductor_Script.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class Conductor_Script : MonoBehaviour
 {
-    protected
     //通電変数がtrueになるかどうか確認する関数
-    void Confimation_Energi()
+    protected void Confimation_Energi()
     {
         //電源と接触してるとき
         if (power_supply_hit == true)
@@ -30,12 +29,18 @@ public class Conductor_Script : MonoBehaviour
                 energization = true;
             }
         }
+
     }
 
     public bool counductor_hit = false;        //導体と当たったか
     public bool insulator_hit = false;         //絶縁体と当たったか
     public bool power_supply_hit = false;      //電源と当たったか
     public bool energization = false;          //通電してるか
+
+    public void Energi_On(bool energi)
+    {
+        energization = energi;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -71,6 +76,11 @@ public class Conductor_Script : MonoBehaviour
     //ここで触れてるオブジェクトをリストにぶち込む
     void OnCollisionStay(Collision c)
     {
+        //if(/*電源ついたよ*/true)
+        //{
+
+        //}
+
         //電源と接触してるとき
         if (c.gameObject.tag == "Power_Supply")
         {
@@ -88,7 +98,7 @@ public class Conductor_Script : MonoBehaviour
         {
             //つながってる導体のenergization(通電確認用変数)で初期化
             bool energi_investigate = c.gameObject.GetComponent<Conductor_Script>().energization;
-           
+
             //Debug.Log(c.gameObject.name);
             //つながっている導体のenergizasionがtrueならこのobjのcounductorhitもtrueにする
             if (energi_investigate == true)
