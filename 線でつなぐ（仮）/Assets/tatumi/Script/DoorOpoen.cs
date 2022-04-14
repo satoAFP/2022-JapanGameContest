4d5c094bfe0d9a8455025a181c306c567e5e0a03
DEV1-11\GA3 <h1202019035@hiratagakuen.onmicrsoft.com>
2022-04-14 11:31:19 +0900

    Linqによるドアのアニメーション制御

--- 線でつなぐ（仮）/Assets/tatumi/Script/DoorOpoen.cs
+++ 線でつなぐ（仮）/Assets/tatumi/Script/DoorOpoen.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class DoorOpoen : MonoBehaviour
 {
-    public GameObject[] ClearTaskObj;
+    public bool[] ClearTaskflag;
+    
+    public Animator anim;
 
 
     // Start is called before the first frame update
@@ -16,6 +19,24 @@ public class DoorOpoen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //‘S—v‘fˆê’v‚©”»’f
+        IEnumerable<bool> results = (IEnumerable<bool>)ClearTaskflag.Distinct();
+        //boolŒ^‚Å”»’è‚Å‚«‚é‚æ‚¤‚É•ÏŠ·
+        foreach (bool Check in results)
+           
+        //—v‘f‚Ìƒ^ƒCƒv‚Æˆê’v‚©‚Ç‚¤‚©‚Ì”»’è
+        if (results.Count() == 1 && Check == true)
+        {
+           anim.SetBool("Open", true);
+           Debug.Log("true");
+        }
+        else 
+        {
+            anim.SetBool("Open", false);
+            Debug.Log("false");
+        }
+
         
+
     }
 }
