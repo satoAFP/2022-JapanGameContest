1d742dc6852148c2c96c6342c9c75f735fe3f73e
DEV3-17\GA3 <h1202019035@haratagakuen.onmicrosoft.com>
2022-04-15 13:31:08 +0900

    多分バグなし

--- 線でつなぐ（仮）/Assets/tatumi/Script/DoorOpoen.cs
+++ 線でつなぐ（仮）/Assets/tatumi/Script/DoorOpoen.cs
@@ -6,15 +6,20 @@ using UnityEngine;
 public class DoorOpoen : MonoBehaviour
 {
     public bool[] ClearTaskflag;
+    private bool taskflag = false;
+    private int num,now_material=0;
 
     
     public Animator anim;
+
+    public Material[] mat = new Material[2];//変更したいマテリアルをセット
+    Material[] mats;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        mats = GetComponent<Renderer>().materials;
     }
 
     // Update is called once per frame
@@ -22,27 +27,36 @@ public class DoorOpoen : MonoBehaviour
     {
         //全要素一致か判断
         IEnumerable<bool> results = (IEnumerable<bool>)ClearTaskflag.Distinct();
+       
+        num = results.Count();
+
         //bool型で判定できるように変換
         foreach (bool Check in results)
-           
-        //要素のタイプと一致かどうかの判定
-        if (results.Count() == 1 && Check == true)
-        {
-           anim.SetBool("Open", true);
-           Debug.Log("true");
-        }
-        else 
-        {
-            anim.SetBool("Open", false);
-            Debug.Log("false");
-        }
 
-        
+        taskflag = Check;
+
+        mats[0] = mat[0];
+
+        GetComponent<Renderer>().materials = mats;
 
     }
 
     public void RayOpenDoor()
     {
+        //ドアが開けるかどうか見てアニメーションを再生
+        if (num == 1 && taskflag == true)
+            anim.SetBool("Open", true);
+        else
+            anim.SetBool("Open", false);
+    }
 
+    public void RayTargetDoor()
+    {
+        //マテリアル変更(1=強調0=普通)
+
+        mats[0] = mat[1];
+
+        GetComponent<Renderer>().materials = mats;
+      
     }
 }
