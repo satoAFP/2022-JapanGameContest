360edf9904715e4ed86b5ec6d9872e759e276bcf
DEV3-17\GA3 <h1202019035@haratagakuen.onmicrosoft.com>
2022-04-18 16:30:22 +0900

    ehuxekuto

--- 線でつなぐ（仮）/Assets/tatumi/Script/Eff_insulator.cs
+++ 線でつなぐ（仮）/Assets/tatumi/Script/Eff_insulator.cs
@@ -7,6 +7,6 @@ public class Eff_insulator : MonoBehaviour
 	private void OnParticleCollision(GameObject other)
 	{
 		// “–‚½‚Á‚½‘Šè‚ğ‹­§“dŒ¹OFF
-		other.gameObject.GetComponent<Conductor_Script>();
+		other.gameObject.GetComponent<Conductor_Script>().Power_Off();
 	}
 }
