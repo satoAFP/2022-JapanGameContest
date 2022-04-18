da4840d3eca4cf2b61e5cddba4a1547c620b33b1
DEV3-22\GA3 <h1202019016@hiratagakuen.onmicrosoft.com>
2022-04-18 16:03:08 +0900

    色移すのおわった

--- 線でつなぐ（仮）/Assets/bonten/Script/MixColor_Script.cs
+++ 線でつなぐ（仮）/Assets/bonten/Script/MixColor_Script.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MixColor_Script : MonoBehaviour
+public class MixColor_Script : Base_Color_Script
 {
-    //色の値の最大値
-    private const int COLOR_MAXNUM = 255;
-    //カラーの数
-    private const int COLOR_RED = 0;    //赤
-    private const int COLOR_GREEN = 1;   //青
-    private const int COLOR_BRUE = 2;  //緑
-    private const int COLOR_MAX = 3;    //要素数
-    private int[] color = new int[COLOR_MAX];
-
+    private bool colorchange_signal = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,23 +16,50 @@ public class MixColor_Script : MonoBehaviour
     {
         
     }
+
     public void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag== "ColorInput")
+        if (collision.gameObject.tag == "ColorInput")
         {
-            color[COLOR_RED]   += collision.gameObject.GetComponent<InputColor_Script>().GetColorRed();
-            color[COLOR_BRUE]  += collision.gameObject.GetComponent<InputColor_Script>().GetColorBlue();
+            color[COLOR_RED] += collision.gameObject.GetComponent<InputColor_Script>().GetColorRed();
+            color[COLOR_BLUE] += collision.gameObject.GetComponent<InputColor_Script>().GetColorBlue();
             color[COLOR_GREEN] += collision.gameObject.GetComponent<InputColor_Script>().GetColorGreen();
-            for(int i=0;i<COLOR_MAX;i++)
+            for (int i = 0; i < COLOR_MAX; i++)
             {
                 //色の値の最大値を超えてたらCOLOR_MAXNUM(255)に固定
-                if (color[i]>COLOR_MAXNUM)
+                if (color[i] > COLOR_MAXNUM)
                 {
                     color[i] = COLOR_MAXNUM;
                 }
             }
+            GetComponent<Renderer>().material.color = new Color32((byte)color[COLOR_RED], (byte)color[COLOR_BLUE], (byte)color[COLOR_GREEN], 1);
+            colorchange_signal = true;
+        }
+    }
 
-            GetComponent<Renderer>().material.color = new Color32((byte)color[COLOR_RED], (byte)color[COLOR_BRUE], (byte)color[COLOR_GREEN], 1);
+    public void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "ColorInput")
+        {
+            //contact_colorinput--;//接触しているColorInputの保存数をはなれた数だけ減らす
+
+            //その後、離れたColorInputが持っている色の値を今これが持ってる色の値から減らす
+            color[COLOR_RED] -= collision.gameObject.GetComponent<InputColor_Script>().GetColorRed();
+            color[COLOR_BLUE] -= collision.gameObject.GetComponent<InputColor_Script>().GetColorBlue();
+            color[COLOR_GREEN] -= collision.gameObject.GetComponent<InputColor_Script>().GetColorGreen();
+
+            GetComponent<Renderer>().material.color = new Color32((byte)color[COLOR_RED], (byte)color[COLOR_BLUE], (byte)color[COLOR_GREEN], 1);
+        }
+    }
+
+    public void OnCollisionStay(Collision collision)
+    {
+        
+        if (collision.gameObject.tag == "ColorOutput" && colorchange_signal == true)
+        {
+            collision.gameObject.GetComponent<OutputColor_Script>().SetMixColor(true);
+            colorchange_signal = false;
+            
         }
     }
 }
