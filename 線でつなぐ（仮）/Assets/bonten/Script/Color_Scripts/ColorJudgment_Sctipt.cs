b8d45d76558d6012202f223588eb8d6cedea50e2
森井 康太 <h1202019016@hiratagakuen.onmicrosoft.com>
2022-05-10 14:31:25 +0900

    色いじってるなう

--- 線でつなぐ（仮）/Assets/bonten/Script/Color_Scripts/ColorJudgment_Sctipt.cs
+++ 線でつなぐ（仮）/Assets/bonten/Script/Color_Scripts/ColorJudgment_Sctipt.cs
@@ -15,18 +15,15 @@ public class ColorJudgment_Sctipt : Base_Color_Script
     // Update is called once per frame
     void Update()
     {
+        //カラーチェンジの信号が送られたら色を変える。
         if(colorchange_signal==true)
         {
             GetComponent<Renderer>().material.color = new Color32((byte)color[COLOR_RED],  (byte)color[COLOR_GREEN], (byte)color[COLOR_BLUE], 1);
             colorchange_signal = false;
         }
 
-        //objs[0].SetActive(true);
-        //objs[1].SetActive(false);
-
         if (clearColor[COLOR_RED] == color[COLOR_RED] && clearColor[COLOR_GREEN] == color[COLOR_GREEN] && clearColor[COLOR_BLUE] == color[COLOR_BLUE])
         {
-            Debug.Log("くりあー");
             //ここにクリアの証的なコード
             objs[0].SetActive(false);
             objs[1].SetActive(true);
