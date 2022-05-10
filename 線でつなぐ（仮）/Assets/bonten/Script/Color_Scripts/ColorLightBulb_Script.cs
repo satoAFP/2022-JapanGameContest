afc85998c50750ef04a6ce60fbd5978f4f7a929b
森井 康太 <h1202019016@hiratagakuen.onmicrosoft.com>
2022-05-10 16:36:02 +0900

    こめんとついか

--- 線でつなぐ（仮）/Assets/bonten/Script/Color_Scripts/ColorLightBulb_Script.cs
+++ 線でつなぐ（仮）/Assets/bonten/Script/Color_Scripts/ColorLightBulb_Script.cs
@@ -10,8 +10,11 @@ public class ColorLightBulb_Script : Base_Color_Script
     // Start is called before the first frame update
     void Start()
     {
+        //ポイントライトobjを取得
         PointLight = transform.Find("Point Light").gameObject;
+        //色を設定
         GetComponent<Renderer>().material.color = new Color32((byte)color[COLOR_RED], (byte)color[COLOR_GREEN], (byte)color[COLOR_BLUE], 1);
+        //ポイントライトに色を入力
         PointLight.GetComponent<Base_Color_Script>().SetColor(color);
         PointLight.gameObject.GetComponent<Light>().color = new Color((byte)color[COLOR_RED], (byte)color[COLOR_GREEN], (byte)color[COLOR_BLUE]);
     }
