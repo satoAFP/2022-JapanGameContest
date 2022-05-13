d59c29e4d9e739290d2de1132639e12205a65dde
Yoshimame <h1202016008@hiratagakuen.onmicrosoft.com>
2022-05-13 15:05:46 +0900

    森井君Script削除

--- 線でつなぐ（仮）/Assets/bonten/Script2/BlockCheck.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BlockCheck : MonoBehaviour
-{
-    [System.NonSerialized] public bool block = true;
-}
--- 線でつなぐ（仮）/Assets/bonten/Script2/ElementNameSetting/NamedArrayAttribute.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class NamedArrayAttribute : PropertyAttribute
-{
-    public readonly string[] names;
-    public NamedArrayAttribute(string[] names) { this.names = names; }
-}
