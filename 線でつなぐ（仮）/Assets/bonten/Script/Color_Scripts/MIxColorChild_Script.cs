79248901191dcdd8496a226c251c5fc1173e83dd
DEV3-22\GA3 <h1202019016@hiratagakuen.onmicrosoft.com>
2022-05-13 15:54:56 +0900

    バグ修正

--- 線でつなぐ（仮）/Assets/bonten/Script/Color_Scripts/MIxColorChild_Script.cs
+++ 線でつなぐ（仮）/Assets/bonten/Script/Color_Scripts/MIxColorChild_Script.cs
@@ -20,12 +20,6 @@ public class MIxColorChild_Script : Base_Color_Script
         SetColor(decolor, colculation);
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
