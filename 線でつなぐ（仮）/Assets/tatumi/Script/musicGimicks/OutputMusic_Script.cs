2edd28ee1135203603996dcef59ff0392c38d4c4
DEV3-17\GA3 <h1202019035@haratagakuen.onmicrosoft.com>
2022-05-16 15:54:54 +0900

    プレハブ改修、音関連

--- 線でつなぐ（仮）/Assets/tatumi/Script/musicGimicks/OutputMusic_Script.cs
+++ 線でつなぐ（仮）/Assets/tatumi/Script/musicGimicks/OutputMusic_Script.cs
@@ -31,9 +31,9 @@ public class OutputMusic_Script : Base_Enegization
         if (MCmode == false)
         {
             if (energization == true)
-                GetComponent<Renderer>().material.color = new Color32(71, 214, 255, 1);
+                GetComponent<Renderer>().material.color = new Color32(71, 214, 255, 200);
             else
-                GetComponent<Renderer>().material.color = new Color32(255, 255, 255, 1);
+                GetComponent<Renderer>().material.color = new Color32(255, 255, 255, 200);
         }
 
     }
