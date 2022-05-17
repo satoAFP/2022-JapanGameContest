2e9d5020242e719bd21c37f6bdb409a743f68b89
DEV3-6\GA3 <h120201907@hiratagakuen.onmicrosoft.com>
2022-05-17 15:30:30 +0900

    フェード処理追加

--- 線でつなぐ（仮）/Assets/sato/script/fade_pop.cs
+++ 線でつなぐ（仮）/Assets/sato/script/fade_pop.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class fade_pop : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField, Header("フェード速度"), Range(0f, 0.02f)] float fade_speed;
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+        // Update is called once per frame
+        void Update()
     {
-        
+        //透過度を上げる
+        gameObject.GetComponent<Image>().color -= new Color(0, 0, 0, fade_speed);
+
+        //完全に透過したら消す
+        if (gameObject.GetComponent<Image>().color.a < 0)
+            gameObject.SetActive(false);
     }
 }
