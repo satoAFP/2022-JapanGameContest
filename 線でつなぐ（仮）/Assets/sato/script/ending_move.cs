7566aa8b296263bd593b69b94a48cde7d0c1fcdc
DEV3-6\GA3 <h120201907@hiratagakuen.onmicrosoft.com>
2022-05-30 13:22:57 +0900

    エンディング作成

---
+++ 線でつなぐ（仮）/Assets/sato/script/ending_move.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ending_move : MonoBehaviour
+{
+    [SerializeField, Header("スタッフロールスピード")] float role_speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //スタッフロールの移動処理
+        if (this.gameObject.GetComponent<RectTransform>().localPosition.y <= 1750)
+            this.gameObject.GetComponent<RectTransform>().localPosition += new Vector3(0, role_speed, 0);
+    }
+}
